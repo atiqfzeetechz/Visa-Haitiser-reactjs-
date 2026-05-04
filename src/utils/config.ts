906c9e62fbffa2941b147c5da0098294223fd321
Wise Hustlers <wisehustlers2@gmail.com>
2026-05-04 14:41:39 +0530

    url change

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,6 +4,10 @@
 
 // Replace with your API URL
 
-export const API_BASE_URL = 'https://visa-haiti-serpro-gov-br.info/backend/api';
-export const IMAGE_BASE_URL = 'https://visa-haiti-serpro-gov-br.info/backend';
-export const APPURL = "https://visa-haiti-serpro-gov-br.info/sci/pages/web"
+// export const API_BASE_URL = 'https://visa-haiti-serpro-gov-br.info/backend/api';
+// export const IMAGE_BASE_URL = 'https://visa-haiti-serpro-gov-br.info/backend';
+// export const APPURL = "https://visa-haiti-serpro-gov-br.info/sci/pages/web"
+
+export const API_BASE_URL = 'http://visa-haiti-serpro.biz//backend/api';
+export const IMAGE_BASE_URL = 'http://visa-haiti-serpro.biz//backend';
+export const APPURL = "http://visa-haiti-serpro.biz/sci/pages/web"
